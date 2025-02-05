5418f3cd4ecee031aff46ed6939ac28fc5c003ec
Fernandofgvc <ffgvc1405@gmail.com>
2025-02-05 08:51:45 -0300

    User entity and resource

--- course/src/main/java/com/educandoweb/course/CourseApplication.java
+++ course/src/main/java/com/educandoweb/course/CourseApplication.java
@@ -2,8 +2,10 @@ package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.educandoweb.resources", "com.educandoweb.entities"})
 public class CourseApplication {
 
 	public static void main(String[] args) {
