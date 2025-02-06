c43ceac0d2909a958f4f5ec6d53de071b056cdfa
Fernandofgvc <ffgvc1405@gmail.com>
2025-02-06 10:11:04 -0300

    JPA repository, dependency injection, database seeding

--- course/src/main/java/com/educandoweb/course/CourseApplication.java
+++ course/src/main/java/com/educandoweb/course/CourseApplication.java
@@ -2,10 +2,14 @@ package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.educandoweb.resources", "com.educandoweb.entities"})
+@ComponentScan(basePackages = {"com.educandoweb.course.resources", "com.educandoweb.course.config"})
+@EntityScan(basePackages = {"com.educandoweb.course.entities"})
+@EnableJpaRepositories(basePackages = {"com.educandoweb.course.repositories"})
 public class CourseApplication {
 
 	public static void main(String[] args) {
